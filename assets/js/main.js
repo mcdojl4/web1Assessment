a6cf52533dfdafe40491917f3b4b76a8ad3bde4c
Josh McDonough <mcdojl4@student.op.ac.nz>
2022-11-18 16:32:56 +1300

    Finished git fetch and placing it in a grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-let api_key = "xffONO9umdoAOkcfbV47hqsQT6Br98hnnkqytpNLDi4"
-let BASE_URL = "https://api.unsplash.com/search/photos/?query=skincare&client_id={CLIENT_ID}";
+const BASE_URL = "https://api.unsplash.com";
 
-fetch(BASE_URL)
+fetch(BASE_URL + '/search/photos/?query=skincare&client_id=xffONO9umdoAOkcfbV47hqsQT6Br98hnnkqytpNLDi4')
       .then(response => response.json())
       .then(data => {
-        console.log(response)
-        // date = data["values"][2]["time"].slice(0,10);
-        // highTime = data["values"][2]["time"].slice(11,19);
-        // lowTime = data["values"][3]["time"].slice(11,19);
-        // highTide = data["values"][2]["value"] + " metres";
-        // lowTide = data["values"][3]["value"] + " metres";
+        // console.log(data)
+        // console.log()
+
+          document.querySelectorAll("#gallery_section > div img").forEach((img,i) => {
+          img.src = data["results"][i]["urls"]["regular"];
+          console.log(data["results"][i]["urls"]["regular"])
+          })
       });
