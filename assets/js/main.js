b0728a047a691c8a3e2470bc729051907e046930
Josh McDonough <mcdojl4@student.op.ac.nz>
2022-11-21 17:44:12 +1300

    Javascript commenting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,21 @@
 const BASE_URL = "https://api.unsplash.com";
+// Setting the button that will run the function menuDropdown using the eventlistener.
 let menuBtn = document.querySelector(".menuBtn").addEventListener("click", menuDropdown);
 let searchBtn = document.querySelector(".searchBtn").addEventListener("click", searchDropdown)
 
-
+//fetching the api url with the search parameters.
 fetch(BASE_URL + '/search/photos/?query=skincare&client_id=xffONO9umdoAOkcfbV47hqsQT6Br98hnnkqytpNLDi4')
       .then(response => response.json())
+      //pushing the data received into a json making it easy to read and navigate.
       .then(data => {
+        //selects each gallery sections divs image and setting the source into the url fetched. 
           document.querySelectorAll("#gallery_section > div img").forEach((img,i) => {
           img.src = data["results"][i]["urls"]["regular"];
           })
       });
       
 function menuDropdown() {
+    //toggles the class show into the menudropdown class list when function is run.
   document.getElementById("menuDropdown").classList.toggle("show");
 }
 
